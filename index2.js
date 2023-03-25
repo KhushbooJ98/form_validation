5d250a553827715688fe6440c7519cfad7741c3c
Khushboo Kumari <126779635+KhushbooJ98@users.noreply.github.com>
2023-03-25 16:54:01 +0530

    helping files

---
+++ index2.js
@@ -0,0 +1,13 @@
+// let num1= 2;
+// let num2 = 02;
+// if(num1==num2){
+//     console.log("true");
+// }else{
+//     console.log("false");
+// }
+
+
+for(let i = 10; i <= 10; i--){
+    document.write(i);
+
+}
